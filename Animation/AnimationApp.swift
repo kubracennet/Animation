20cc453bf7d93f4dc094bad0520b541fbd1126ba
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-08 23:30:20 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Kübra Cennet Yavaşoğlu on 8.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
